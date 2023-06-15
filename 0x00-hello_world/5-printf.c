ff8ecde36cfbc607d6c0fd8609685470f9f47c64
root <root@TONY>
2023-06-15 20:02:55 +0300

    c print using prinf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main -Starting point of any c program
+  *
+  *Description: Starting point of any c program
+  *
+  *Return: returns an arguement
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
