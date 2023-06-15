ed3aa99759b8978cd9617088881bad0f01fddf8b
root <root@TONY>
2023-06-16 00:54:13 +0300

    rm file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
 
-/**
- *main - starting point of the c program
- *
- * Description: prints the given text without using puts or printf function
- *
- * Return: always return the argument in the main function
- */
-
-int main(void)
-{
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-}
