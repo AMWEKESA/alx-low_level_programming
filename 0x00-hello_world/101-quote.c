94f37b8d7f252c5183f37b91302ce78c38108fca
root <root@TONY>
2023-06-16 01:04:25 +0300

    use write to print in c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
+#include <stdi.h>
 
+/**
+  *main - starting point
+  *description: prints output
+  *return: return an int
+  */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
