8a33c7c1663828b0fdd315aeb6819bd988dc62bd
root <root@TONY>
2023-06-16 19:54:36 +0300

    print lowercase char with exception of others

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+# include <stdion.h>
+
+/**
+  * main - strtn pt
+  *
+  * Description: print alphabets in lower except q n e
+  *
+  * Return: return 0
+  */
+
+int main(void)
+{
+	int a = 97;
+
+	do {
+		if (a != 101 && a != 113)
+		{
+		putchar (a);
+		}
+	} while (a <= 122);
+
+	putchar ('\n');
+
+	return (0);
+}
