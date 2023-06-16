742cbd4c33aeb21336c6cd7c838a207bbb8c1bcc
root <root@TONY>
2023-06-17 01:03:43 +0300

    combo of 3 no.s

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - starting pt
 *
@@ -11,13 +12,13 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	for (j = i + 1; j <= '9'; i++)
+	for (j = i + 1; j <= '9'; j++)
 	{
 	for (k = j + 1; k <= '9'; k++)
 	{
-	if ((j != i) != k)
+	if ((j != i) && (k != i) && (k != j))
 	{
 	putchar(i);
 	putchar(j);
