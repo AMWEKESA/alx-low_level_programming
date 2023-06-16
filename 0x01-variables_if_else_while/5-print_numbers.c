7e21800ffb4df6367cd6f0906f527b2723f5f3e1
root <root@TONY>
2023-06-16 20:16:33 +0300

    prints base 10 no.s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - starting pt
+*
+* Description: prints
+*
+*Return: returns an int val
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	do {
+		printf("%d", i;)
+		i++;
+	} while (i < 10);
+	return (0);
+}
