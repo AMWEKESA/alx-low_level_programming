de5e2b406077468dd044348c9c83827ca629ad11
root <root@TONY>
2023-06-16 20:59:31 +0300

    pritn hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+<F5>#include <stdio.h>
+/**
+* main - starting pt
+*
+* Description: prints
+*
+*Return: returns an int val
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j = 97;
+
+	do {
+		putchar(i);
+		i++;
+	} while (i <= 57);
+
+	do {
+		putchar(j);
+		j++;
+	} while (j <= 102);
+	putchar('\n');
+
+	return (0);
+}
