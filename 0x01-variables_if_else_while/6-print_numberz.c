f267c9ff5987308eb0de573c260a6f3a31ae48ea
root <root@TONY>
2023-06-16 20:28:00 +0300

    prints base 10 no.s using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - starting pt
+*
+* Description: prints base no.s using putcha()
+*
+*Return: returns an int val
+ */
+
+int main(void)
+{
+	int i = 48
+
+	do {
+		putchar(a);
+		i++;
+	} while (a <= 57);
+	putchar('\n');
+	return (0);
+}
