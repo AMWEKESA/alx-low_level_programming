038eb0a69968da336c561e5830cddca89771ddbd
root <root@TONY>
2023-06-16 20:42:55 +0300

    prints lowercase alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - starting pt
+*
+* Description: prints lowercase alphabet in reverse
+*
+*Return: returns an int val
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	do {
+		putchar(i);
+		i--;
+
+	} while (i => 97);
+	return (0);
+}
