3cb2644b3067e8310433429f7b94a562698e4800
root <root@TONY>
2023-06-21 02:55:04 +0300

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,15 +21,15 @@ void times_table(void)
 
 			if (y == 0)
 			{
-			_putchar(z + '0');
+				_putchar(z + '0');
 			}
-			if (z < 10 && y != 0)
+			else if (z < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(z = '0');
-			} else if (z >= 10)
+				_putchar(z + '0');
+			} else
 			{
 				_putchar(',');
 				_putchar(' ');
