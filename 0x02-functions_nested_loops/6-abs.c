31362c767ed9ab8533416ef21b11ad20f1f48a52
root <root@TONY>
2023-06-21 00:49:35 +0300

    returns the absolute val of parameter

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _abs - prints the absolute val of an int
+*
+*@n: int to be checked
+*
+*int: data type to be returned
+*
+* Return: val of int
+*/
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	{
+		return (-n);
+	}
+
+}
