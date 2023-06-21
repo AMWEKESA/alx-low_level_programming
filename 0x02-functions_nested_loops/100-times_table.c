61ba973abeaab09e122919bafae514b48b7407a1
root <root@TONY>
2023-06-21 14:24:58 +0300

    print times 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - prints times table,
+ * @n: int whose multiples are to computed
+ */
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n >= 0 && n < 15)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			for (x = 0; x <= n; x++)
+			{
+				z = y * x;
+				if (z < 10)
+				{
+					if (x > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				} else if (z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
