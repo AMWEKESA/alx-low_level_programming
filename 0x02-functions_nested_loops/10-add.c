469066ef6c319bada919e393a4b2a0fb90fd9682
root <root@TONY>
2023-06-21 03:10:06 +0300

    compute sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - computes the sum of two numbers
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: resultsturn results
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
