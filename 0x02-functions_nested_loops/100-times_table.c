7dac642b2d568031336941c6ea332a1553fcd3a0
root <root@TONY>
2023-06-21 14:38:17 +0300

    correct times 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (n >= 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (y = 0; y <= n; y++)
 		{
@@ -20,11 +20,11 @@ void print_times_table(int n)
 					{
 						_putchar(',');
 						_putchar(' ');
+						_putchar(' ');
 					}
 					_putchar(' ');
-					_putchar(' ');
 					_putchar(z + '0');
-				} else if (z < 100)
+				} else if (z >=10 && z <= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -35,7 +35,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((z / 10) + '0');
+					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
