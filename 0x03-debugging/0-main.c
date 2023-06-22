3cc89e72bc46e54ccc6607a975e55f97e978e4d4
root <root@TONY>
2023-06-22 14:16:16 +0300

    debugging 001

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,12 @@
 * main - tests function testing if integer is positive or negative
 * Return: Always Success
 */
+
 int main(void)
 {
-        int i;
+	int i;
 
-        i =0;
-        positive_or_negative(i);
-
+	i = 0;
+	positive_or_negative(i);
 	return (0);
 }
