94dd0a1586760c087190a8c491fc9920c9d22785
root <root@TONY>
2023-06-22 17:00:30 +0300

    multiplies 2 no.s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - get the product of 2 int
+* @x: int to be multiplied by y
+* @y: int to be miltpied by x
+* Return: always success
+*/
+
+int mul(int x, int y)
+{
+	int prod;
+
+	prod = x * y;
+	return (prod);
+}
