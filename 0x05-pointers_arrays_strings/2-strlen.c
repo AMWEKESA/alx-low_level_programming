b766a7b6331d2af62b22cf31bdc3dc3cff48cc15
AMWAKESA <thonymsai1>
2023-06-27 02:13:30 +0300

    string lengt

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - retund length of a string
+ * @s: string whos length is t be retuned
+ * Return: returns 0
+ */
+
+int _strlen(char *s)
+{
+	int long = 0;
+
+	while (long != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
