37406d8d60a5de0ab002ccfb81e367ae287af51f
AMWAKESA <thonymsai1>
2023-06-27 02:27:30 +0300

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int _strlen - retund length of a string
+ * @str: string whos length is t be retuned
+ * Return: returns
+ */
+int _strlen(char *str);
+{
+	while (str != '\0')
+	{
+		_putchar(*str++);				}
+		_putchar('\n');
+}
