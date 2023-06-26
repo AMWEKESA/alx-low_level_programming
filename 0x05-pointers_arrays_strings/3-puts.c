357854648db66b5a7bb6faf1446c246c63749514
AMWAKESA <thonymsai1>
2023-06-27 02:33:58 +0300

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * int _strlen - retund length of a string
+ * _puts - retund length of a string
  * @str: string whos length is t be retuned
  * Return: returns
  */
-int _strlen(char *str)
+int _puts(char *str)
 {
 	while (str != '\0')
 	{
