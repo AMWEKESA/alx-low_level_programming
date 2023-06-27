bd3b68ce4cee730a502bd4e00ec105b27f8dd78e
AMWAKESA <thonymsai1>
2023-06-28 00:37:21 +0300

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,16 +9,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i, x = 0;
+	int i = 0;
 
-	while (src + i != '\0')
+	while (src[i] != '\0')
 	{
-		i++;
-	}
-	for ( ; x < i ; x++)
-	{
-		dest[x] = src[x];
-	}
+		dest[i] = src[i];
+	}	i++;
+
 	dest[i] = '\0';
+
 	return (dest);
 }
