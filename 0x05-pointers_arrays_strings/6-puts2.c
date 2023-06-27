b9b55e1cd4694a963dd4ef7164dbd4005fd67f57
AMWAKESA <thonymsai1>
2023-06-27 23:23:01 +0300

    prints elemets of a string frm the 1st one

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - write characters of a string starting wit 1st
+ * @str: string to be written
+ * Return: returns
+ */
+
+void puts2(char *str)
+{
+	int i, length = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		length++;
+	}
+	for (i = 0; i <= length)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
