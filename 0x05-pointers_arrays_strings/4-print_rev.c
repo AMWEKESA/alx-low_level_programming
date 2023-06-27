b566c75595ff59a4f150cd170e04826f28559cd9
AMWAKESA <thonymsai1>
2023-06-27 03:00:46 +0300

    in reverse git push
    git add \
    
    betty 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - ptints str in rev
+ * @s: char to be revesed
+ * Return: returns
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++
+	}
+	s--;
+	for (o = longi; O > 0 O--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
