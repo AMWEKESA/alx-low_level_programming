c301d2a8626567e5cdb32e5d80e33cf8a4b1095a
AMWAKESA <thonymsai1>
2023-06-27 20:06:34 +0300

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: sting to be reversed
+ * Return: returns 0
+ */
+
+void rev_string(char *s)
+{
+	int i, length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
