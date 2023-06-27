88bf6453c798aa8d3058aa71e4af32bf781f607d
AMWAKESA <thonymsai1>
2023-06-28 00:00:12 +0300

    print half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half od string
+ * @str: string to be printed
+ * Return: returns
+ */
+
+void puts_half(char *str)
+{
+	int start_index;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+		if (len % 2 == 0)
+		{
+			start_index = len / 2;
+
+		}
+		else
+		{
+			start_index = (len + 1 + / 2;
+		}
+		for (i = start_index; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+}
