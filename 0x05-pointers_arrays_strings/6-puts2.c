ac403b645af8f3d01d1c59273b53f79057e767e5
AMWAKESA <thonymsai1>
2023-06-27 23:31:13 +0300

    prints elemets of a string frm the 1st one

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,14 @@
 
 void puts2(char *str)
 {
-	int i, length = 0;
+	int i = 0;
 
-	while (*str != '\0')
-	{
-		str++;
-		length++;
-	}
-	for (i = 0; i <= length; i++)
+	while (*str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
+		i++;
 	}
 }
