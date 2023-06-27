950fb93dd8102f598974f4cda1f717ce7eb41048
AMWAKESA <thonymsai1>
2023-06-28 00:30:29 +0300

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcpy - coppies string pointed to src
+ * @dest: where to copy the files
+ * @src: where filea are coppied from
+ * Return: returns
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, x = 0;
+
+	while ((src + i) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < i ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
