43ef4047bae43f7c740abf20965ec3984f3d3058
AMWAKESA <thonymsai1>
2023-06-28 00:44:27 +0300

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * *_strcpy - coppies string pointed to src
- * @dest: where to copy the files
+ * *_strcpy - coppies string pointed src
+ * @dest: where to copy the files to
  * @src: where filea are coppied from
- * Return: returns
+ * Return: 0
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int x = 0;
 
-	while (src[i] != '\0')
+	while (*(src + i) != '\0')
 	{
-		dest[i] = src[i];
-	}	i++;
-
+		i++;
+	}
+	for ( ; x < i ; x++)
+	{
+		dest[x] = src[x];
+	}
 	dest[i] = '\0';
-
 	return (dest);
 }
