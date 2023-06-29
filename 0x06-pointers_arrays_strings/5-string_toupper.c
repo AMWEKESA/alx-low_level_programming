c99ee67ca17b10fb52211dc5f07e47af0ead9552
AMWAKESA <thonymsai1>
2023-06-30 01:41:07 +0300

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @n: pointer
+ * Return: success
+ */
+
+char string_toupper(char *)
+{
+	if (str == NULL)
+		return;
+
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+			*str -= 32;
+		str++;
+	}
+}
+
