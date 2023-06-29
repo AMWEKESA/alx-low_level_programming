e448088521c3cbcf07a96a05a51b4cfd50c297a5
AMWAKESA <thonymsai1>
2023-06-29 18:15:52 +0300

    concatenate 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenates two strings
+ * @src: string to be conatenated wit dest
+ * @dest: string to be concatenated with src
+ * Return: returns
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; dest[i] != '\0'; i++, len++)
+	{
+		src[len] = dest[i];
+	}
+	src[len] = '\0';
+	return (src);
+}
