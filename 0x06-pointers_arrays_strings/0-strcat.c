05f88726c07fdb743a4cdd8b2aec5d4c945b8b7e
AMWAKESA <thonymsai1>
2023-06-30 12:07:32 +0300

    concatenate review

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,14 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int i;
+	int i = 0;
 
 	int len = 0;
 
-	while (src[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		dest[len] = src[i];
-		len++;
-		i++;
-	}
-	i = 0;
 	while (src[i] != '\0')
 	{
 		dest[len] = src[i];
