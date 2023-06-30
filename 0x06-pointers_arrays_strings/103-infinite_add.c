2a510d68409506e343b4a775a5a6d89a108cd06d
AMWAKESA <thonymsai1>
2023-06-30 14:00:51 +0300

    buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * infinite_add - adds 2 numbers
+ * @n1: int to be added to n2
+ * @n2: int to be added to n1
+ * Return: returns
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	while (size_r != '\0')
+	{
+		r = n1 + n2;
+	}
+}
