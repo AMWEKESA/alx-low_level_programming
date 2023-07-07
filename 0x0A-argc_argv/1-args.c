25133776bbd9e6cf3059b8b75f628d324e7a2dcf
AMWAKESA <thonymsai1>
2023-07-07 23:28:36 +0300

    prints its name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * @argc: argument counter
+  * @argv: argument vector
+  *
+  *Return: return success
+  *
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
