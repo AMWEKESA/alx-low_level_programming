1ea1480c3ada0292506b9c3b4d45a48b67f12cfe
AMWAKESA <thonymsai1>
2023-07-17 18:04:05 +0300

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - prints file name
+  *
+  * FILE: predefined macro for filename
+  *
+  * Return: void
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
