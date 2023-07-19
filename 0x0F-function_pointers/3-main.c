a337c9a9053bd747b0e86f35485fe634687a1558
AMWAKESA <thonymsai1>
2023-07-19 19:16:55 +0300

    3 correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error!\n");
+		printf("Error\n");
 		return (1);
 	}
 
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	if (get_op_func(op) == NULL)
 	{
-		printf("Error!\n");
+		printf("Error\n");
 		return (2);
 	}
 
