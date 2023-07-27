222b6188b50eff1f42b933fdfa422cc7a999bdf6
Your Git Username <your.email@example.com>
2023-07-28 01:38:32 +0300

    add at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * add_node_end - add new elements at the end of a linked list
+  *
+  * @head: pointer to lists head
+  * @str: string to be added
+  *
+  * Return: pointer to str, else NULL
+  */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nod;
+	list_t *tem = *head;
+	unsigned int len;
+
+	len = 0;
+	do {
+		++len;
+	} while (str[len] != '\0');
+
+	nod = malloc(sizeof(list_t));
+	if (!nod)
+		return (NULL);
+
+	nod->str = strdup(str);
+	nod->len = len;
+	nod->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nod;
+		return (nod);
+	}
+	while (tem->next)
+	{
+		tem = tem->next;
+
+	tem->next = nod;
+	}
+	return (nod);
+}
