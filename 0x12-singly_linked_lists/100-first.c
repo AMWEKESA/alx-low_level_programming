5b38eceb8243b500249fc7582bc21c1601808c92
Your Git Username <your.email@example.com>
2023-07-28 02:35:44 +0300

    print before calling main() betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void print_sent(void) __attribute__((constructor));
+
 /**
   * print_sent - prints a sentence
   *
@@ -7,10 +9,8 @@
   *
   */
 
-void print_sent(void) __attribute__((constructor));
-
 void print_sent(void)
 {
-	printf("You're beat! and yet, you must allow,\n")"
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
