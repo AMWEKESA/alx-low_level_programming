263353154ad755cfb8eb808515df506e5ec03eb6
AMWEKESA <thonymsai1@gmail.com>
2023-11-03 23:12:05 +0300

    correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,19 +1,17 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update hash table
- * 
- * @ht: pointer to hash table
- * @key: The key to add
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
  *
  * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *hs;
+	hash_node_t *new;
 	char *value_copy;
 	unsigned long int index, i;
 
@@ -35,13 +33,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
-	hs = malloc(sizeof(hash_node_t));
-	if (hs == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
 		free(value_copy);
 		return (0);
 	}
-	hs->key = strdup(key);
+	new->key = strdup(key);
 	if (new->key == NULL)
 	{
 		free(new);
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,11 +4,10 @@
  * hash_table_delete - Deletes a hash table.
  * @ht: A pointer to a hash table.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
-	hash_node_t *node, *sub;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
@@ -18,11 +17,11 @@ void hash_table_delete(hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				sub = node->next;
+				tmp = node->next;
 				free(node->key);
 				free(node->value);
 				free(node);
-				node = sub;
+				node = tmp;
 			}
 		}
 	}
